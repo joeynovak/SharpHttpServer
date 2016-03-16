d422944c6dcabd56fb91f8d1c558aeb647680d4d
EvAlex <evalexhimself@gmail.com>
2016-03-17 01:06:19 +0300

    add demo console app

---
+++ SharpHttpServer.DemoConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Qoollo.SharpHttpServer.DemoConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting HttpServer...");
+
+            var server = new Server(8080);
+            server.Run();
+
+            Console.WriteLine("HttpServer started.");
+            Console.WriteLine("Listening at {0}", server.BaseUrl);
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to exit");
+            Console.ReadLine();
+
+            server.Stop();
+        }
+    }
+}
