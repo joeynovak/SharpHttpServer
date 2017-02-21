093999b4af65838e9ebfbeba0aeace1c982284a8
joey <joey.novak@gmail.com>
2017-02-21 13:07:56 -0700

    Demo Server uses https, and has an api method to view the client certificate (as opposed to the server certificate and usually not present).

--- SharpHttpServer.DemoConsoleApp/Server.cs
+++ SharpHttpServer.DemoConsoleApp/Server.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Qoollo.SharpHttpServer.DemoConsoleApp
@@ -13,21 +14,36 @@ namespace Qoollo.SharpHttpServer.DemoConsoleApp
         public Server(int port)
             :base(port)
         {
+            Scheme = "https";       
             Get["/"] = _ => "Hello world!";
 
             Get["/api/v1/users"] = GetUsers;
+
+            Get["/api/v1/certificate"] = Certificate;
 
             ServeStatic(new DirectoryInfo("html"), "static");
         }
 
         private string GetUsers(HttpListenerRequest arg)
         {
+                           
             return JsonConvert.SerializeObject(new[]
             {
                 new { Id = 1, Username = "peter" },
                 new { Id = 1, Username = "jack" },
-                new { Id = 1, Username = "john" },
+                new { Id = 1, Username = "john" }
             });
+        }
+
+        private string Certificate(HttpListenerRequest arg)
+        {
+            X509Certificate2 certificate = arg.GetClientCertificate();
+            if (certificate != null)
+                return JsonConvert.SerializeObject("Certificate Thumbprint :" + certificate.Thumbprint);
+            else
+            {
+                return JsonConvert.SerializeObject("No Certificate Provided");
+            }
         }
     }
 }
